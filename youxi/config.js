1a6963a17f285aa3599cf8478c533f559a01a20a
ktqx <hahagw2023+github@gmail.com>
2026-06-28 21:18:17 +0800

    Create config.js

---
+++ youxi/config.js
@@ -0,0 +1,6 @@
+// config.js - 聚合游戏大厅节目单
+window.GAMES_LIST = [
+  { id: "flip-card",     name: "🎰 幸运大翻牌", file: "games/flip-card.js" },
+  { id: "red-envelope",  name: "🧧 疯狂抢红包", file: "games/red-envelope.js" }
+  // 👈 以后想加第3、第4个游戏，直接在这下面加一行，别的地方动都不用动！
+];
